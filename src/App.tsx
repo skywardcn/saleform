1f40108031f019ef2a3be3b712251db015ff7939
skywardcn <hjg16@163.com>
2023-12-04 22:28:01 +0800

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { CompChange } from 'features/sale'
+import { SaleForm } from './features/saleForm'
 
 function App() {
 
   return (
     <>
       <CompChange></CompChange>
+      <SaleForm></SaleForm>
     </>
   )
 }
